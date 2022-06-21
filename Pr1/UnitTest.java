a56393bdd56fd3b76da17180c8610502733cc3a3
Eduard <iampolskiy.exceedteam@gmail.com>
2022-06-21 15:48:31 +0300

    Pr1

---
+++ Pr1/UnitTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class UnitTest
+{
+    @Test
+    public void shouldReturnCorrectValue() {
+        assertEquals(1, Main.getMetreFromCentimetre(100), 0);
+    }
+
+    @Test
+    public void shouldReturnZeroValue() {
+        assertEquals(0, Main.getMetreFromCentimetre(0), 0);
+    }
+
+    @Test
+    public void shouldReturnFloatValue() {
+        assertEquals(1, Main.getMetreFromCentimetre(105), 0);
+    }
+}
