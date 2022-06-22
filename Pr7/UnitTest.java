90a7d03ef69ea53e7a1fd1e9021ed3949cdde9bf
Eduard <iampolskiy.exceedteam@gmail.com>
2022-06-22 14:48:20 +0300

    Pr7

---
+++ Pr7/UnitTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class UnitTest {
+    @Test
+    public void shouldReturnCorrectValue() { assertEquals("5.5", Main.tenPercent(Main.test1));}
+
+    @Test
+    public void shouldReturnZeroValue() {
+        assertEquals("-11.0", Main.tenPercent(Main.test2));
+    }
+
+    @Test
+    public void shouldReturnFloatValue() {
+        assertEquals("Why do you want to get a percentage of 0?", Main.tenPercent(Main.test3));
+    }
+}
