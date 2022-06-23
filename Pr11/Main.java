0b549a194f26fa1e14e23e5e85a67c4a51d9dbb4
Eduard <iampolskiy.exceedteam@gmail.com>
2022-06-23 12:15:38 +0300

    Pr11

---
+++ Pr11/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int result = 1;
+        for (int i = 1; i < 11; i++) {
+            result *= i;
+        }
+        System.out.println(result);
+    }
+}
