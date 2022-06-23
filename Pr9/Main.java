2bab650c21a3f6167594964a53273d39b70295ad
Eduard <iampolskiy.exceedteam@gmail.com>
2022-06-23 11:42:38 +0300

    Pr9 edited

---
+++ Pr9/Main.java
@@ -0,0 +1,9 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(1 + 2 * (3 + 4 * 5 + 6 * 7 + 8 * 9 + 10 * 11 + 12 * 13 + 14 )+ 15);
+    }
+}
