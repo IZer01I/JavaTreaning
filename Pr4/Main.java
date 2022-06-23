ac30cc0bd2be3096e5b5103a020d733af3750ab5
Eduard <iampolskiy.exceedteam@gmail.com>
2022-06-23 10:34:35 +0300

    Pr4 edited

--- Pr4/Main.java
+++ Pr4/Main.java
@@ -1,12 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getVolume(25, 5, 2));
+        System.out.println("pool volume: " + getVolume(25, 5, 2));
     }
 
-    public static String getVolume(int a, int b, int c) {
-
-          double x = (a * b * c) * 1000;
-        if(x <= 0) return "pool volume cannot be less than or equal to 0";
-        else return ("pool volume: " + x);
+    public static int getVolume(int a, int b, int c) {
+        int x = (a * b * c) * 1000;
+        if (a <= 0 || b <= 0 || c <= 0) throw new ArithmeticException("pool volume cannot be less than or equal to 0");
+        else return x;
     }
 }
--- Pr4/UnitTest.java
+++ Pr4/UnitTest.java
@@ -1,18 +1,22 @@
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertThrows;
 
-public class UnitTest
-{
-    @Test
-    public void shouldReturnCorrectValue() { assertEquals("pool volume cannot be less than or equal to 0", Main.getVolume(1, -5, 5));}
+public class UnitTest {
 
-    @Test
-    public void shouldReturnZeroValue() { assertEquals("pool volume cannot be less than or equal to 0", Main.getVolume(100, 5, 0));
+    @Test(expected = ArithmeticException.class)
+    public void ArithmeticExceptionValue() {
+        assertEquals("pool volume cannot be less than or equal to 0", Main.getVolume(1, 2, -5));
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void ArithmeticExceptionZeroValue() {
+        assertEquals("pool volume cannot be less than or equal to 0", Main.getVolume(100, 5, 0));
     }
 
     @Test
-    public void shouldReturnFloatValue() {
-        assertEquals("pool volume: 6000.0", Main.getVolume(1, 2, 3));
+    public void shouldReturnIntValue() {
+        assertEquals(6000, Main.getVolume(1, 2, 3));
     }
 }
