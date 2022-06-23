40108154b6f3f59fa2943b908703fdffa77e3cb3
Eduard <iampolskiy.exceedteam@gmail.com>
2022-06-23 11:57:56 +0300

    Pr10

---
+++ Pr10/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        Zerg zerg1 = new Zerg();
+        Zerg zerg2 = new Zerg();
+        Zerg zerg3 = new Zerg();
+        Zerg zerg4 = new Zerg();
+        Zerg zerg5 = new Zerg();
+        Zerg zerg6 = new Zerg();
+        Zerg zerg7 = new Zerg();
+        Zerg zerg8 = new Zerg();
+        Zerg zerg9 = new Zerg();
+        Zerg zerg10 = new Zerg();
+
+
+        Protoss protoss1 = new Protoss();
+        Protoss protoss2 = new Protoss();
+        Protoss protoss3 = new Protoss();
+        Protoss protoss4 = new Protoss();
+        Protoss protoss5 = new Protoss();
+
+
+        Terran terran1 = new Terran();
+        Terran terran2 = new Terran();
+        Terran terran3 = new Terran();
+        Terran terran4 = new Terran();
+        Terran terran5 = new Terran();
+        Terran terran6 = new Terran();
+        Terran terran7 = new Terran();
+        Terran terran8 = new Terran();
+        Terran terran9 = new Terran();
+        Terran terran10 = new Terran();
+        Terran terran11 = new Terran();
+        Terran terran12 = new Terran();
+    }
+}
