b3f35d6649a5434b4cd0d52d148ba69c5dac25ba
Eduard <iampolskiy.exceedteam@gmail.com>
2022-06-24 11:15:06 +0300

    Pr14 edited

--- Pr14/UnitTest.java
+++ Pr14/UnitTest.java
@@ -3,12 +3,12 @@ import org.junit.Test;
 import static junit.framework.TestCase.assertEquals;
 
 public class UnitTest {
-    @Test(expected = ArithmeticException.class)
-    public void FirstTest() {assertEquals("Your action is illogical!", Main.convertToSeconds(0));}
+    @Test
+    public void FirstTest() {assertEquals(0, Main.convertToSeconds(0));}
 
     @Test
     public void SecondTest() {assertEquals(18000, Main.convertToSeconds(5));}
 
-    @Test(expected = ArithmeticException.class)
-    public void ThirdTest() {assertEquals("Your action is illogical!", Main.convertToSeconds(-2));}
+    @Test
+    public void ThirdTest() {assertEquals(0, Main.convertToSeconds(-2));}
 }
